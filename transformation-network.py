63b87d2f0d3e4b298557437046be4fb84eaa087c
Yichen Jiang <pauljyc0929@gmail.com>
2018-04-27 01:09:13 -0400

    change where model is defined

--- transformation-network.py
+++ transformation-network.py
@@ -74,12 +74,6 @@ def _conv_transpose_layer(x, num_filters, kernal_size, strides, padding='same', 
 parser = build_parser()
 options = parser.parse_args()
 style_path = options.style
-
-# get tensor representations of our images
-# content_path = options.content
-# output_path = options.output
-# content_image = K.variable(preprocess_image(content_path))
-
 style_image = K.variable(preprocess_image(style_path))
 
 # # this will contain our generated image
@@ -89,88 +83,89 @@ style_image = K.variable(preprocess_image(style_path))
 #     combination_image = K.placeholder((1, img_x, img_y, 3))
 
 class LossCalculator:
-    def __init__(self):
-        pass
+    def __init__(self, style_img):
+        self.vgg19 = vgg19.VGG19(weights='imagenet', include_top=False)
+        self.style_img = style_img
 
-    def custom_loss_wrapper(style_img):
-        def custom_loss(content_img, combination_img):
-            # compute the neural style loss
-            # first we need to define 4 util functions
+    def custom_loss(self, content_img, combination_img):
+        # compute the neural style loss
+        # first we need to define 4 util functions
 
-            input_tensor = K.concatenate([content_img,
-                                          style_img,
-                                          combination_img], axis=0)
+        input_tensor = K.concatenate([content_img,
+                                      self.style_img,
+                                      combination_img], axis=0)
 
-            # build the VGG16 network with our 3 images as input
-            # the model will be loaded with pre-trained ImageNet weights
-            vgg = vgg19.VGG19(input_tensor=input_tensor,
-                                weights='imagenet', include_top=False)
-            print('Model loaded.')
+        # build the VGG16 network with our 3 images as input
+        # the model will be loaded with pre-trained ImageNet weights
+        vgg = self.vgg19(input_tensor=input_tensor)
+        # # TODO: separate model definition from loss calculation
+        # vgg = vgg19.VGG19(input_tensor=input_tensor,
+        #                     weights='imagenet', include_top=False)
+        print('Model loaded.')
 
-            # get the symbolic outputs of each "key" layer (we gave them unique names).
-            outputs_dict = dict([(layer.name, layer.output) for layer in vgg.layers])
+        # get the symbolic outputs of each "key" layer (we gave them unique names).
+        outputs_dict = dict([(layer.name, layer.output) for layer in vgg.layers])
 
-            def gram_matrix(x):
-                assert K.ndim(x) == 3
-                if K.image_data_format() == 'channels_first':
-                    features = K.batch_flatten(x)
-                else:
-                    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
-                gram = K.dot(features, K.transpose(features))
-                return gram
+        def gram_matrix(x):
+            assert K.ndim(x) == 3
+            if K.image_data_format() == 'channels_first':
+                features = K.batch_flatten(x)
+            else:
+                features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+            gram = K.dot(features, K.transpose(features))
+            return gram
 
-            # the "style loss" is designed to maintain
-            # the style of the reference image in the generated image.
-            # It is based on the gram matrices (which capture style) of
-            # feature maps from the style reference image
-            # and from the generated image
-            def style_loss(style, combination):
-                assert K.ndim(style) == 3
-                assert K.ndim(combination) == 3
-                S = gram_matrix(style)
-                C = gram_matrix(combination)
-                channels = 3
-                size = img_x * img_y
-                return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
+        # the "style loss" is designed to maintain
+        # the style of the reference image in the generated image.
+        # It is based on the gram matrices (which capture style) of
+        # feature maps from the style reference image
+        # and from the generated image
+        def style_loss(style, combination):
+            assert K.ndim(style) == 3
+            assert K.ndim(combination) == 3
+            S = gram_matrix(style)
+            C = gram_matrix(combination)
+            channels = 3
+            size = img_x * img_y
+            return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
 
-            # an auxiliary loss function
-            # designed to maintain the "content" of the
-            # base image in the generated image
-            def content_loss(base, combination):
-                return K.sum(K.square(combination - base))
+        # an auxiliary loss function
+        # designed to maintain the "content" of the
+        # base image in the generated image
+        def content_loss(base, combination):
+            return K.sum(K.square(combination - base))
 
-            # the 3rd loss function, total variation loss,
-            # designed to keep the generated image locally coherent
-            def total_variation_loss(x):
-                assert K.ndim(x) == 4
-                if K.image_data_format() == 'channels_first':
-                    a = K.square(x[:, :, :img_x - 1, :img_y - 1] - x[:, :, 1:, :img_y - 1])
-                    b = K.square(x[:, :, :img_x - 1, :img_y - 1] - x[:, :, :img_x - 1, 1:])
-                else:
-                    a = K.square(x[:, :img_x - 1, :img_y - 1, :] - x[:, 1:, :img_y - 1, :])
-                    b = K.square(x[:, :img_x - 1, :img_y - 1, :] - x[:, :img_x - 1, 1:, :])
-                return K.sum(K.pow(a + b, 1.25))
+        # the 3rd loss function, total variation loss,
+        # designed to keep the generated image locally coherent
+        def total_variation_loss(x):
+            assert K.ndim(x) == 4
+            if K.image_data_format() == 'channels_first':
+                a = K.square(x[:, :, :img_x - 1, :img_y - 1] - x[:, :, 1:, :img_y - 1])
+                b = K.square(x[:, :, :img_x - 1, :img_y - 1] - x[:, :, :img_x - 1, 1:])
+            else:
+                a = K.square(x[:, :img_x - 1, :img_y - 1, :] - x[:, 1:, :img_y - 1, :])
+                b = K.square(x[:, :img_x - 1, :img_y - 1, :] - x[:, :img_x - 1, 1:, :])
+            return K.sum(K.pow(a + b, 1.25))
 
-            # combine these loss functions into a single scalar
-            loss = K.variable(0.)
-            layer_features = outputs_dict['block5_conv2']
-            base_image_features = layer_features[0, :, :, :]
+        # combine these loss functions into a single scalar
+        loss = K.variable(0.)
+        layer_features = outputs_dict['block5_conv2']
+        base_image_features = layer_features[0, :, :, :]
+        combination_features = layer_features[2, :, :, :]
+        loss += content_weight * content_loss(base_image_features,
+                                              combination_features)
+
+        feature_layers = ['block1_conv1', 'block2_conv1',
+                          'block3_conv1', 'block4_conv1',
+                          'block5_conv1']
+        for layer_name in feature_layers:
+            layer_features = outputs_dict[layer_name]
+            style_features = layer_features[1, :, :, :]
             combination_features = layer_features[2, :, :, :]
-            loss += content_weight * content_loss(base_image_features,
-                                                  combination_features)
-
-            feature_layers = ['block1_conv1', 'block2_conv1',
-                              'block3_conv1', 'block4_conv1',
-                              'block5_conv1']
-            for layer_name in feature_layers:
-                layer_features = outputs_dict[layer_name]
-                style_features = layer_features[1, :, :, :]
-                combination_features = layer_features[2, :, :, :]
-                sl = style_loss(style_features, combination_features)
-                loss += (style_weight / len(feature_layers)) * sl
-            loss += total_variation_weight * total_variation_loss(combination_img)
-            return loss
-        return custom_loss
+            sl = style_loss(style_features, combination_features)
+            loss += (style_weight / len(feature_layers)) * sl
+        loss += total_variation_weight * total_variation_loss(combination_img)
+        return loss
 
 
 # load the microsoft COCO image dataset
@@ -189,8 +184,8 @@ for imageName in sorted(os.listdir("training/train2014")):
 img_train = np.asarray(imList, dtype="float32")
 print(img_train.shape)
 
-# define loss function
-loss_calculator = LossCalculator
+# define loss calculator
+loss_calculator = LossCalculator(style_image)
 
 # reshape the data into a 4D tensor - (sample_number, x_img_size, y_img_size, num_channels)
 # because the MNIST is greyscale, RGB colour images would have 3
@@ -224,9 +219,9 @@ output = Activation('tanh')(deconv3)
 
 # Train
 model = Model(inputs=input1, outputs=output)
-model.compile(loss=loss_calculator.custom_loss_wrapper(style_image),
+model.compile(loss=loss_calculator.custom_loss(),
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 model.fit(x=img_train, y=img_train, batch_size=batch_size, epochs=epochs, verbose=1)
 
-model.save('transfer_model_complete.h5')
+model.save('transfer_model_partial.h5')
