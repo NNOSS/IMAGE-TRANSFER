6b528d83f1573516f85c1d79ed6619b6e9522a16
SeunginLyu <seungin.lyu@students.olin.edu>
2018-05-06 16:48:49 -0400

    Input = output

--- transformation-network.py
+++ transformation-network.py
@@ -215,9 +215,9 @@ print(img_train.shape[0], 'train samples')
 
 input1 = Input(shape=input_shape);
 # Convolutional Layers
-conv1 = _conv_layer(input1, num_filters=32, kernal_size=(9,9), strides=(1,1), input_shape=input_shape)
-conv2 = _conv_layer(conv1, num_filters=64, kernal_size=(3,3), strides=(2,2))
-conv3 = _conv_layer(conv2, num_filters=128, kernal_size=(3,3), strides=(2,2))
+# conv1 = _conv_layer(input1, num_filters=32, kernal_size=(9,9), strides=(1,1), input_shape=input_shape)
+# conv2 = _conv_layer(conv1, num_filters=64, kernal_size=(3,3), strides=(2,2))
+# conv3 = _conv_layer(conv2, num_filters=128, kernal_size=(3,3), strides=(2,2))
 
 #Residual blocks
 # res1 = _residual_block(conv3, 3)
@@ -227,13 +227,14 @@ conv3 = _conv_layer(conv2, num_filters=128, kernal_size=(3,3), strides=(2,2))
 # res5 = _residual_block(res4, 3)
 
 # Conv2DTranspose / Deconvolutional layers
-deconv1 = _conv_transpose_layer(conv3, num_filters=64, kernal_size=(3,3), strides=(2,2))
-deconv2 = _conv_transpose_layer(deconv1, num_filters=32, kernal_size=(3,3), strides=(2,2))
-output = _conv_layer(deconv2, num_filters=3, kernal_size=(9,9), strides=(1,1), padding="same", relu=False)
+# deconv1 = _conv_transpose_layer(conv3, num_filters=64, kernal_size=(3,3), strides=(2,2))
+# deconv2 = _conv_transpose_layer(deconv1, num_filters=32, kernal_size=(3,3), strides=(2,2))
+# output = _conv_layer(deconv2, num_filters=3, kernal_size=(9,9), strides=(1,1), padding="same", relu=False)
 # pred = Activation('tanh')(deconv3)
 # output = Add()([pred, input1])
 # output = Activation('tanh')(output)
 # output = Lambda(lambda x: x*127.5 + 255./2)(output)
+output = input1
 
 # Train
 model = Model(inputs=input1, outputs=output)
